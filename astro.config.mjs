1d431f0c35826f031654bc2cc1ff7ee7a26a194b
Gonzalo Azaldegi <98758892+AzalDevX@users.noreply.github.com>
2025-02-12 00:11:37 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,16 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    mode: 'directory',
+    functionPerRoute: true,
+  }),
   integrations: [tailwind()],
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['node:async_hooks'], // Esto puede ser necesario para evitar errores con Firebase
+      },
+    },
+  },
 });
